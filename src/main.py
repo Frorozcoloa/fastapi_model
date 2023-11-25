4669753d755ce6771d28e66bfbcfcc8b9c98dda3
frorozcoloa <freorozcoloa@gmail.com>
2023-11-25 00:17:48 -0500

    [ADD] All test are good

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI, HTTPException, Depends, Header
+from fastapi import FastAPI, HTTPException, Depends
+from contextlib import asynccontextmanager
 from fastapi.security import APIKeyHeader
-from fastapi.exceptions import RequestValidationError
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 from typing import List
 import torch
 
@@ -10,24 +8,29 @@ from .config import API_CONFIG, MODEL_CONFIG
 from .predict import DoubleItStrategy
 from .schema import InferenceInput, InferenceOutput, InferenceReponse
 
+# x-api-key header
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+ml_model = {}
+
+
+# Upload model on startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # upload model
+    model = DoubleItStrategy(MODEL_CONFIG["model_path"])
+    ml_model["model"] = model
+    yield
+    ml_model.clear()
+
+
 # initialize FastAPI
 app = FastAPI(
     title="Tenpo Challenge",
     description="API para realizar predicciones con un modelo de PyTorch",
     version="0.0.1",
+    lifespan=lifespan,
 )
-
-
-# x-api-key header
-api_key_header = APIKeyHeader(name="X-API-Key")
-
-
-# Upload model on startup
-@app.on_event("startup")
-async def startup_event():
-    # Carga el modelo
-    model = DoubleItStrategy(MODEL_CONFIG["model_path"])
-    app.package = {"model": model, "api_key_header": api_key_header}
 
 
 # verified api key
@@ -44,7 +47,7 @@ async def predict(
 ) -> InferenceReponse:
     try:
         # get model from package
-        result = app.package["model"].main(data.data)
+        result = ml_model["model"].main(data.data)
         inference = InferenceOutput(result=result)
         inference_response = InferenceReponse(error=False, results=inference)
 
--- test/test_endpoint.py
+++ test/test_endpoint.py
@@ -1,26 +1,38 @@
 from fastapi.testclient import TestClient
 from src.main import app
+from pathlib import Path
+from dotenv import dotenv_values
+
+env_path = Path(".") / ".env"
+
+config = dotenv_values(env_path)
 
 
 def test_predict_with_valid_data():
-    client = TestClient(app)
-    API_KEY = "your-api-key"
-    data = [1.0, 2.0, 3.0, 4.0]
-    headers = {"X-API-Key": API_KEY}
+    with TestClient(app) as client:
 
-    response = client.post("/predict", headers=headers, json=data)
-    result = response.json()
+        # Mock data for the request
+        data = {"data": [1.0, 2.0, 3.0]}
+        headers = {
+            "accept": "application/json",
+            "X-API-Key": config["API_KEY"],  # Replace with your actual API key
+            "Content-Type": "application/json",
+        }
 
-    assert response.status_code == 200
-    assert "result" in result
-    assert result["result"] == [2.0, 4.0, 6.0, 8.0]
+        response = client.post("/predict", headers=headers, json=data)
+
+        assert response.status_code == 200
+        assert "results" in response.json()
+        assert response.json()["results"]["result"] == [2.0, 4.0, 6.0]
+        assert response.json()["error"] == False
 
 
 def test_predict_with_invalid_api_key():
     client = TestClient(app)
-    API_KEY = "your-api-key"
     data = [1.0, 2.0, 3.0, 4.0]
-    headers = {"X-API-Key": "invalid-api-key"}
+    headers = {"X-API-Key": "invalid-api-key",
+               "Content-Type": "application/json",
+               "Accept": "application/json"}
 
     response = client.post("/predict", headers=headers, json=data)
 
@@ -29,10 +41,13 @@ def test_predict_with_invalid_api_key():
 
 def test_predict_with_invalid_data():
     client = TestClient(app)
-    API_KEY = "your-api-key"
+    API_KEY = config["API_KEY"]
     data = "invalid-data"  # Data should be a list of floats
-    headers = {"X-API-Key": API_KEY}
+    headers = {"X-API-Key": API_KEY,
+               "Content-Type": "application/json",
+               "Accept": "application/json"}
 
     response = client.post("/predict", headers=headers, json=data)
 
     assert response.status_code == 422  # 422 Unprocessable Entity
+
